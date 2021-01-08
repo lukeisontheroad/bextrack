d884a9ef063deb9f8d9d4f24f533a8973bd397f0
Lukas Schönbächler <l.schoenbaechler@papers.ch>
2021-01-08 22:17:33 +0100

    chore(build): version 1.5

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,14 @@
 export const environment = {
-  production: false,
+  production: true,
   useHash: true,
   auth_config: {
     client_id: '2ac5b8a5-9795-4c8c-a09b-fb3c32e93cc7',
     server_host: 'https://idp.bexio.com',
-    redirect_url: 'xyz.lukeisontheroad.bexio://callback',
+    redirect_url: 'http://localhost:8100/auth/callback',
+    redirect_url_native: 'xyz.lukeisontheroad.bexio://callback',
     response_type: 'token',
-    end_session_redirect_url: 'xyz.lukeisontheroad.bexio://endsession',
+    end_session_redirect_url: 'http://localhost:8100/auth/end-session',
+    end_session_redirect_url_native: 'xyz.lukeisontheroad.bexio://endsession',
     scopes: 'openid profile email monitoring_edit project_show offline_access', // contact_show
     pkce: false
   }
