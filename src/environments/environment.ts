6de173ce5863e79c7132c98da1bcc19849970b38
Lukas Schönbächler <l.schoenbaechler@papers.ch>
2021-04-08 23:14:54 +0200

    feat(github-pages): add www folder for github pages

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,28 +1,15 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
-  production: false,
-  useHash: false,
+  production: true,
+  useHash: true,
   auth_config: {
     client_id: '2ac5b8a5-9795-4c8c-a09b-fb3c32e93cc7',
     server_host: 'https://idp.bexio.com',
-    redirect_url: 'http://localhost:8100/auth/callback',
+    redirect_url: 'https://app.bextrack.com/auth/callback',
     redirect_url_native: 'xyz.lukeisontheroad.bexio://callback',
     response_type: 'token',
-    end_session_redirect_url: 'http://localhost:8100/auth/end-session',
+    end_session_redirect_url: 'https://app.bextrack.com/auth/end-session',
     end_session_redirect_url_native: 'xyz.lukeisontheroad.bexio://endsession',
     scopes: 'openid profile email monitoring_edit project_show offline_access', // contact_show
     pkce: false
   }
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
